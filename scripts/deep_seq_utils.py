68da9a466aa4dbce6ec589fe91f751f672b99f43
Hugh Haddox <haddox@uw.edu>
2018-04-22 16:40:11 -0700

    Add script for computing EC50s from incomplete NGS data

--- scripts/deep_seq_utils.py
+++ scripts/deep_seq_utils.py
@@ -58,7 +58,7 @@ def original_read_fastq(fn):
 def read_fastq(filename):
     """
     Compute the number of counts for each unique protein sequence in an input FASTQ file
-    
+
     This function is the same as `original_read_fastq`, but I changed it so that it is somewhat
     more memory efficient. Specifically, instead of reading the whole FASTQ file into memory, it
     iterates over it line by line.
@@ -111,11 +111,69 @@ def read_fastq(filename):
 
     return (filename, counts_dict)
 
+def read_fastq_only_forward_read(filename, length_cds_to_parse=102):
+    """
+    Compute the number of counts for each unique protein sequence in an input
+    FASTQ file with a single read that doesn't cover the whole coding sequence
+
+    This function is the same as `read_fastq`, but I changed it so that it is
+    able to analyze incomplete deep-sequencing data, specifically related to
+    a scenario where you have a single forward read that doesn't cover the entire
+    coding sequence.
+
+    Args:
+        `filename`: the name of the FASTQ file
+        `length_cds_to_parse`: the number of nucleotides of the coding sequence
+            to parse from each deep-sequencing read, where the start of the
+            coding sequence is definied by the 5' flanking restriction cut site,
+            the sequence of which is hard coded below. (default: 102)
+
+    Returns:
+        A tupple of (`filename`, `counts_dict`):
+            `filename` : same as input
+            `counts_dict` : a dictionary with counts for each unique protein sequence,
+                of the form {protein sequence}:{counts}
+    """
+
+    # Go through each of the FASTQ file, parse sequences of interest, and add them to a dictionary
+    # that tracks the counts of each
+    counts_dict = {}
+    with open(filename) as file:
+        for (i, line) in enumerate(file):
+
+            # Each entry is a set of four lines. The below line of code skips over the header
+            # line, i.e., the first one in each set
+            if i % 4 != 1: continue
+
+            # ... it also skips over lines that are less than 12 characters
+            if len(line) < 12: continue
+
+            # Look for the pattern "CATATG" in the line in the forward direction, find it's
+            # position, and incriment that position by six, setting this equal to the starting
+            # point of the sequence of interest. Otherwise, if the pattern cannot be found, skip
+            # this entry. Note: find always identifies the first position where the pattern
+            # occurs in the string
+            if 'CATATG' in line:
+                startpt = line.find('CATATG')+6
+            else:
+                continue
+
+            endpt = startpt + length_cds_to_parse
+            coding_dna = Seq(line[startpt:endpt], generic_dna)
+            protein_seq = str(coding_dna.translate(to_stop=True))
+            if protein_seq == '':
+                continue
+            if protein_seq not in counts_dict:
+                counts_dict[protein_seq] = 0
+            counts_dict[protein_seq] += 1
+
+    return (filename, counts_dict)
+
 
 def read_fastq_with_c_script(filename, c_script_path):
     """
     Performs the same thing as the above functions for parsing FASTQ files, but using a C script written by Luki Goldschmidt
-    
+
     We are interested in seeing if this C-based program is much faster than the Python based ones above.
 
     The `-p` flag tells the program to return counts for amino-acid sequences instead of DNA sequences
@@ -123,7 +181,7 @@ def read_fastq_with_c_script(filename, c_script_path):
     Args:
         `filename`: same as the function `read_fastq`
         `c_script_path`: the path to the C script called `parse_fastq`
-        
+
     Returns: same as the function `read_fastq`
     """
 
@@ -148,42 +206,48 @@ def read_fastq_with_c_script(filename, c_script_path):
     return (filename, counts_dict)
 
 
-def ComputeCounts(fastq_files_and_output_file):
+def ComputeCounts(fastq_files_and_output_file, forward_read_only=False):
     """
     Compute protein counts from a list of FASTQ files and write the counts to a file
-    
+
     This function computes protein counts from a list of FASTQ files, aggregating the results
     among all FASTQ files. I use this function to parallelize this process. I combine multiple
     variables in a single input tupple, which makes the parallelization command much simpler.
     Currently, it uses the above function `read_fastq` to compute counts, which is more memory
     efficient than the function originally used in the lab `original_read_fastq`.
-    
+
     Args:
         `fastq_files_and_outputprefix`: A tupple of the following variables:
-        
+
             `fastq_files: A list of paths to input FASTQ files.
 
             `outputfile`: The name the output CSV file reporting sequence counts.
-        
+
+        `forward_read_only`: If the FASTQ data only has a single forward read
+            (default: False)
+
     Returns:
         A CSV of sequence counts. The first line of this file is a header:
-            
+
             `sequence,counts`
-        
+
         while the remaining lines are comma-delimited entries of these variables:
-            
+
             `{sequence},{counts}`
     """
-    
+
     # Unpack the input
     (fastq_files, output_file) = fastq_files_and_output_file
-    
+
     # Compute counts for each FASTQ file
     assert len(fastq_files) > 0, "The list of FASTQ files is empty"
     counts_per_file = {}
     for (i, fastq_file) in enumerate(fastq_files):
-        counts_per_file[i] = read_fastq(fastq_file)[1] # return the dictionary of counts
-    
+        if not forward_read_only:
+            counts_per_file[i] = read_fastq(fastq_file)[1] # return the dictionary of counts
+        else:
+            counts_per_file[i] = read_fastq_only_forward_read(fastq_file)[1]
+
     # Aggregate counts acorss all FASTQ files, or just return the one dictionary if there is only one input
     # FASTQ file
     if len(fastq_files) == 1:
@@ -197,29 +261,29 @@ def ComputeCounts(fastq_files_and_output_file):
                     counts_dict[seq] = counts_per_file[i][seq]
                 else:
                     counts_dict[seq] += counts_per_file[i][seq]
-    
+
     # Write the results to an output file
     counts_df = pandas.DataFrame(list(counts_dict.items()), columns=['sequence', 'counts'])
     counts_df.to_csv(output_file, index=False)
-    
+
 
 def ParsePAREOutfile(outfile):
     """
     This function parses the output data generated by PARE when assemblying paired-end reads
-    
+
     Args:
         `outfile`: the path to a file with the output data generated by PARE
-        
+
     Returns:
         A tupple with the following three variables in the order they appear in the below list:
             `n_assembled_reads` : the total number of assembled reads
             `n_discarded_reads` : the total number of discarded reads
             `n_non_assembled_reads` : the total number of non_assembled_reads
     """
-    
+
     # Pattern used to extract data
     n_reads_pattern = re.compile(r'\: (?P<n_reads>[\d,]+) /')
-    
+
     # Use regular expressions to extract the relevant info from the file
     n_assembled_reads = n_discarded_reads = n_non_assembled_reads = n_total_reads = None
     with open(outfile) as f:
@@ -230,17 +294,17 @@ def ParsePAREOutfile(outfile):
                     raise ValueError("Already found data for `n_assembled_reads`")
                 n_assembled_match = re.search(n_reads_pattern, line)
                 n_assembled_reads = int(n_assembled_match.group('n_reads').replace(',', ''))
-                
+
             elif 'Discarded reads .' in line:
                 if n_discarded_reads:
                     raise ValueError("Already found data for `n_discarded_reads`")
                 n_discarded_match = re.search(n_reads_pattern, line)
                 n_discarded_reads = int(n_discarded_match.group('n_reads').replace(',', ''))
-                
+
             elif 'Not assembled reads .' in line:
                 if n_non_assembled_reads:
                     raise ValueError("Already found data for `n_non_assembled_reads`")
                 n_non_assembled_match = re.search(n_reads_pattern, line)
                 n_non_assembled_reads = int(n_non_assembled_match.group('n_reads').replace(',', ''))
-    
+
     return (n_assembled_reads, n_discarded_reads, n_non_assembled_reads)
